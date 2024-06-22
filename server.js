19637b69368d7c7501c05d8d66a09c1a74e54563
José Vitor Alves Pignataro <52422803+Vitorpignataro@users.noreply.github.com>
2024-06-22 15:41:16 -0300

    updating

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Vamos amigo');
+  res.send('Funciona pelo amor de Deus');
 });
 
 app.listen(PORT, HOST, () => {
