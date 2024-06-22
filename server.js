ddcde0e097d1804cd2b5f12e9fbd55514d252dbb
José Vitor Alves Pignataro <52422803+Vitorpignataro@users.noreply.github.com>
2024-06-22 01:13:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('alan-careco');
+  res.send('testando deploy');
 });
 
 app.listen(PORT, HOST, () => {
